fb134b8669ae67c5790790690a3a4f61f168fef3
josebin0530-create <josebin0530@gmail.com>
2026-06-04 14:19:25 +0900

    메인수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,13 @@ import './App.css';
 
 export default function App() {
   const [panelOpen, setPanelOpen] = useState(false);
-  const [isReturning, setIsReturning] = useState(false);
 
   const handleEnter = useCallback(() => setPanelOpen(true), []);
-  const handleClose = useCallback(() => {
-    setIsReturning(true);
-    setPanelOpen(false);
-    setTimeout(() => setIsReturning(false), 100);
-  }, []);
 
   return (
     <>
-      <Home onEnter={handleEnter} panelOpen={panelOpen} isReturning={isReturning} />
-
-      {/* 배경 딤 */}
-      <div
-        className={`about-backdrop${panelOpen ? ' open' : ''}`}
-        onClick={handleClose}
-        aria-hidden="true"
-      />
-
-      <AboutMe open={panelOpen} onClose={handleClose} />
+      <Home onEnter={handleEnter} panelOpen={panelOpen} />
+      <AboutMe open={panelOpen} />
     </>
   );
 }
